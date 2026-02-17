a5996f2d29d24826a3ed50fcb7a994b52302631a
menagagandhin-droid <menagagandhin@gmail.com>
2026-02-17 11:11:22 +0530

    Classify input value as Low, Medium, or High
    Fix the condition for medium classification.

---
+++ Classify Value as Low, Medium, or High.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a<=100)
+{
+    printf("LOW");
+}
+else if(a>=101>=300)
+{
+    printf("MEDIUM");
+}
+else
+{
+    printf("HIGH");
+}
+}
