3b37f328ffa6cb934deec073a9a0d88fdc09f97a
Menaga Gandhi N <menagagandhin@gmail.com>
2026-02-28 15:58:40 +0530

    Create Program 63.c

---
+++ Program 63.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double base, exponent, result;
+
+    printf("Enter base value: ");
+    scanf("%lf", &base);
+
+    printf("Enter exponent value: ");
+    scanf("%lf", &exponent);
+
+    result = pow(base, exponent);
+
+    printf("Result: %.2lf\n", result);
+
+    return 0;
+}
