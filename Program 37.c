506292d5ff75ca27c36803a9405d90c7634831df
Menaga Gandhi N <menagagandhin@gmail.com>
2026-02-28 14:36:48 +0530

    Add Program 37 for checking divisibility

---
+++ Program 37.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num, divisor;
+    printf("Enter the number to be checked: ");
+    scanf("%d", &num);
+    printf("Enter the divisor: ");
+    scanf("%d", &divisor);
+    if (divisor == 0) {
+        printf("Error: Division by zero is not allowed.\n");
+    } 
+    else {
+        if (num % divisor == 0) {
+            printf("%d is divisible by %d\n", num, divisor);
+        } 
+        else {
+            printf("%d is not divisible by %d\n", num, divisor);
+        }
+    }
+
+    return 0;
+}
